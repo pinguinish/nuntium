b276bffbce42b64021310ea1e7e295ff1604b7cf
zaferlerim <alijon.alimov.english@gmail.com>
2023-10-31 21:42:10 +0300

    feat(uikit): add icons

--- uikit/example/example.dart
+++ uikit/example/example.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:uikit/const/nuntium_icons.dart';
 
 void main(List<String> args) => runApp(const ExampleUIKit());
+
+List<IconData> icons = const [
+  NuntiumIcons.angelRight,
+  NuntiumIcons.arrowLeft,
+  NuntiumIcons.book,
+  NuntiumIcons.bookmark,
+  NuntiumIcons.categories,
+  NuntiumIcons.envelope,
+  NuntiumIcons.eye,
+  NuntiumIcons.home,
+  NuntiumIcons.microphone,
+  NuntiumIcons.padlock,
+  NuntiumIcons.person,
+  NuntiumIcons.search,
+  NuntiumIcons.share,
+  NuntiumIcons.signout,
+];
+
+const verticalGap = SizedBox(height: 24);
 
 class ExampleUIKit extends StatelessWidget {
   const ExampleUIKit({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: SingleChildScrollView(
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: 24),
-                Text(
+                verticalGap,
+                const Text(
                   "Nuntium UIKit",
                   style: TextStyle(
                     fontFamily: 'SFProDisplay',
                     fontSize: 24,
                   ),
+                ),
+                verticalGap,
+                Wrap(
+                  spacing: 20,
+                  alignment: WrapAlignment.center,
+                  children: icons.map((i) => Icon(i)).toList(),
                 ),
               ],
             ),
---
+++ uikit/lib/const/nuntium_icons.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/widgets.dart';
+
+class NuntiumIcons {
+  NuntiumIcons._();
+
+  static const _kFontFam = 'NuntiumIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData categories =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData angelRight =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrowLeft =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData book =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bookmark =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData envelope =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData eye =
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home =
+      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData microphone =
+      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData padlock =
+      IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData person =
+      IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData search =
+      IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData share =
+      IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData signout =
+      IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
