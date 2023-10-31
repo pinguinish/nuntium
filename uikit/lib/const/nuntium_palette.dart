02c97b789d2474aa446c4c4ee7c112824f711abc
zaferlerim <alijon.alimov.english@gmail.com>
2023-10-31 11:48:18 +0300

    feat(uikit): add the app's palette

---
+++ uikit/lib/const/nuntium_palette.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract class NuntiumPalette {
+  static const purpleDark = Color(0xFF2536A7);
+  static const purplePrimary = Color(0xFF475AD7);
+  static const purpleLight = Color(0xFF8A96E5);
+  static const purleLighter = Color(0xFFEEF0FB);
+  static const blackDart = Color(0xFF22242F);
+  static const blackPrimary = Color(0xFF333647);
+  static const blackLight = Color(0xFF44485F);
+  static const blackLighter = Color(0xFF555A77);
+  static const greyDart = Color(0xFF666C8E);
+  static const greyPrimary = Color(0xFF7C82A1);
+  static const greyLight = Color(0xFFACAFC3);
+  static const greyLighter = Color(0xFFF3F4F6);
+}
